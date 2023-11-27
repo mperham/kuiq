0d20a7ad08c28d2f34fdaa35cc100a32af7c50d5
Mike Perham <mperham@gmail.com>
2023-11-27 13:12:22 -0800

    Initial pass at i18n

--- kuiq.gemspec
+++ kuiq.gemspec
@@ -12,7 +12,7 @@ Gem::Specification.new do |spec|
   spec.description = "A native desktop application for Sidekiq operators, using the Glimmer toolkit"
   spec.homepage = "https://github.com/mperham/quick"
   spec.license = "LGPL-3.0"
-  spec.required_ruby_version = ">= 2.7.0"
+  spec.required_ruby_version = ">= 3.0.0"
 
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = spec.homepage
--- lib/kuiq/sidekiq_ui.rb
+++ lib/kuiq/sidekiq_ui.rb
@@ -11,8 +11,55 @@ module Kuiq
   class SidekiqUI
     include Glimmer::LibUI::Application
 
+    LOCALES = "./locales"
+
+    # Dont know anything about Glimmer::LibUI::Application!?
+    # Does it have a logger?
+    def logger
+      Sidekiq.logger
+    end
+
+    # Use Sidekiq's i18n with locale files in sidekiq/web/locales
+    # Note task in Rakefile to refresh locale files.
+    def current_locale
+      @locale ||= begin
+        x = (ENV["LANGUAGE"] || ENV["LANG"] || "en").downcase.tr("_", "-")
+        loop do
+          break "en" if x.size < 2
+          break x if File.exist?("#{LOCALES}/#{x}.yml")
+          # dumb brute force heuristic: look for locale files
+          # that match the longest LANG prefix, necessary to serve
+          # more complex lang groups like zh and pt.
+          x = x[0...-1]
+        end
+      end
+    end
+
+    def t(msg, options = {})
+      string = strings(current_locale)[msg] || strings("en")[msg] || msg
+      if options.empty?
+        string
+      else
+        string % options
+      end
+    end
+
+    private def strings(lang)
+      @strings ||= {}
+      @strings[lang] ||= [LOCALES].each_with_object({}) do |path, global|
+        Dir["#{path}/#{lang}.yml"].each do |file|
+          strs = YAML.safe_load_file(file)
+          global.merge!(strs[lang])
+        end
+      end
+    end
+
     before_body do
+      logger.info { "Welcome to Kuiq #{Kuiq::VERSION}, using the #{current_locale.upcase} locale" }
+      logger.info { RUBY_DESCRIPTION }
+
       @job_manager = Model::JobManager.new
+      logger.info { "Redis client #{RedisClient::VERSION}, server #{@job_manager.redis_info["redis_version"]}" }
     end
 
     after_body do
@@ -23,7 +70,7 @@ module Kuiq
       window("Sidekiq UI", WINDOW_WIDTH, WINDOW_HEIGHT) {
         vertical_box {
           tab {
-            tab_item("Dashboard") {
+            tab_item(t("Dashboard")) {
               dashboard(job_manager: @job_manager)
             }
             # TODO enable each tab when implemented (only implemented ones will be visible)
@@ -31,7 +78,7 @@ module Kuiq
             #           }
             #           tab_item("Queues") {
             #           }
-            tab_item("Retries") {
+            tab_item(t("Retries")) {
               retries(job_manager: @job_manager)
             }
             #           tab_item("Scheduled") {
